8b3d0a04e6d3bca3334bc69da3c64a3962576183
Anjali86881 <anjalivaddemani@gmail.com>
2025-06-12 11:24:11 +0530

    Add files via upload

---
+++ ArraySort.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void Sort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+			
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int[] Nums = Arrays.copyOf(numbers, n);
+        Arrays.sort(Nums);
+        System.out.println("Sorted using Arrays.sort(): " + Arrays.toString(Nums));
+    }
+}
